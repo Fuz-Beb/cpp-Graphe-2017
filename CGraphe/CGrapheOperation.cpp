83cd44cbc7420c8cabd91b3e5e395d17d49b57bf
Bebo <pierrick.bobet@cryptolab.net>
2017-05-10 16:04:57 +0200

    Update surcharge

--- CGraphe/CGrapheOperation.cpp
+++ CGraphe/CGrapheOperation.cpp
@@ -1,5 +1,5 @@
 #include "CGrapheOperation.h"
-
+ 
 /*****************************
 Constructeur par défaut
 ******************************
@@ -11,7 +11,7 @@ Entraine : l'objet en cours est initialisé
 CGrapheOperation::CGrapheOperation() : CGraphe()
 {
 }
-
+ 
 /*****************************
 Constructeur de recopie
 ******************************
@@ -23,7 +23,7 @@ Entraine : l'objet en paramètre est recopié et initialisé dans un nouvel objet
 CGrapheOperation::CGrapheOperation(CGrapheOperation & GRAgraphe) : CGraphe(GRAgraphe)
 {
 }
-
+ 
 /*****************************
 Constructeur de confort
 ******************************
@@ -35,7 +35,21 @@ Entraine : l'objet en cours est initialisé
 CGrapheOperation::CGrapheOperation(unsigned int uiNbSommets, unsigned int uiNbArcs) : CGraphe(uiNbSommets, uiNbArcs)
 {
 }
-
+ 
+/*****************************
+Surcharge de l'operateur =
+******************************
+Entrée : CGrapheOperation & GRAGrapheOperation
+Necessité : néant
+Sortie : CGrapheOperation &
+Entraine : modification de l'objet a gauche du signe
+*****************************/
+CGrapheOperation & CGrapheOperation::operator=(CGrapheOperation & GRAGrapheOperation)
+{
+    (CGraphe)*this = (CGraphe) GRAGrapheOperation;
+    return *this;
+}
+ 
 /*****************************
 Destructeur par défaut
 ******************************
@@ -47,7 +61,7 @@ Entraine : l'objet est détruit
 CGrapheOperation::~CGrapheOperation()
 {
 }
-
+ 
 /*****************************
 Methode : Inverser un graphe
 ******************************
@@ -58,17 +72,17 @@ Entraine : le graphe de sortie est inverse
 *****************************/
 CGrapheOperation * CGrapheOperation::GRAInverserGraphe()
 {
-	CGrapheOperation * newGraphe = new CGrapheOperation();
-	unsigned int uiBuffer;
-
-	// Creation des sommets
-	for (unsigned int uiBoucleSommets = 0 ; uiBoucleSommets < GRAGetNbSommets() ; uiBoucleSommets++)
-		newGraphe->GRAAjoutSommet(GRAGetSommets()[uiBoucleSommets]->SOMGetNum(), nullptr, nullptr);
-	// Creation des arcs
-	for (unsigned int uiBoucleSommets = 0 ; uiBoucleSommets < GRAGetNbSommets() ; uiBoucleSommets++)
-		for (unsigned int uiBoucleArcs = 0 ; uiBoucleArcs < GRAGetSommets()[uiBoucleSommets]->SOMGetNbrArcPartant() ; uiBoucleArcs++) {
-			uiBuffer = GRAGetSommets()[uiBoucleSommets]->SOMGetListArcPartant()[uiBoucleArcs]->ARCGetDestination();
-			newGraphe->GRAAjoutArc(GRAGetSommets()[uiBoucleSommets]->SOMGetNum(), newGraphe->GRATrouverSommet(uiBuffer));
-	}
-	return newGraphe;
+    CGrapheOperation * newGraphe = new CGrapheOperation();
+    unsigned int uiBuffer;
+ 
+    // Creation des sommets
+    for (unsigned int uiBoucleSommets = 0 ; uiBoucleSommets < GRAGetNbSommets() ; uiBoucleSommets++)
+        newGraphe->GRAAjoutSommet(GRAGetSommets()[uiBoucleSommets]->SOMGetNum(), nullptr, nullptr);
+    // Creation des arcs
+    for (unsigned int uiBoucleSommets = 0 ; uiBoucleSommets < GRAGetNbSommets() ; uiBoucleSommets++)
+        for (unsigned int uiBoucleArcs = 0 ; uiBoucleArcs < GRAGetSommets()[uiBoucleSommets]->SOMGetNbrArcPartant() ; uiBoucleArcs++) {
+            uiBuffer = GRAGetSommets()[uiBoucleSommets]->SOMGetListArcPartant()[uiBoucleArcs]->ARCGetDestination();
+            newGraphe->GRAAjoutArc(GRAGetSommets()[uiBoucleSommets]->SOMGetNum(), newGraphe->GRATrouverSommet(uiBuffer));
+    }
+    return newGraphe;
 }
--- CGraphe/CGrapheOperation.h
+++ CGraphe/CGrapheOperation.h
@@ -1,64 +1,73 @@
 #ifndef C_GRAPHEOPERATION_H
 #define C_GRAPHEOPERATION_H
-
+ 
 #include "CGraphe.h"
-#include "CSommet.h"
-
+ 
 class CGrapheOperation : public CGraphe
 {
 public:
-	// Constructeurs / Destructeurs
-
-	/*****************************
-	Constructeur par défaut
-	******************************
-	Entrée : néant
-	Necessité : néant
-	Sortie : néant
-	Entraine : l'objet en cours est initialisé
-	*****************************/
-	explicit CGrapheOperation();
-
-	/*****************************
-	Constructeur de recopie
-	******************************
-	Entrée : CGrapheOperation & GRAgraphe
-	Necessité : néant
-	Sortie : néant
-	Entraine : l'objet en paramètre est recopié et initialisé dans un nouvel objet
-	*****************************/
-	explicit CGrapheOperation(CGrapheOperation & GRAgrapheOperation);
-
-	/*****************************
-	Constructeur de confort
-	******************************
-	Entrée : unsigned int uiNbSommets, unsigned int uiNbArcs
-	Necessité : néant
-	Sortie : néant
-	Entraine : l'objet en cours est initialisé
-	*****************************/
-	explicit CGrapheOperation(unsigned int uiNbSommets, unsigned int uiNbArcs);
-
-	/*****************************
-	Destructeur par défaut
-	******************************
-	Entrée : néant
-	Necessité : néant
-	Sortie : néant
-	Entraine : l'objet est détruit
-	*****************************/
-	~CGrapheOperation();
-
-	// Methodes
-
-	/*****************************
-	Methode : Inverser un graphe
-	******************************
-	Entrée : neant
-	Necessité : neant
-	Sortie : CGrapheOperation *
-	Entraine : le graphe de sortie est inverse
-	*****************************/
-	CGrapheOperation * GRAInverserGraphe();
+    // Constructeurs / Destructeurs
+ 
+    /*****************************
+    Constructeur par défaut
+    ******************************
+    Entrée : néant
+    Necessité : néant
+    Sortie : néant
+    Entraine : l'objet en cours est initialisé
+    *****************************/
+    explicit CGrapheOperation();
+ 
+    /*****************************
+    Constructeur de recopie
+    ******************************
+    Entrée : CGrapheOperation & GRAgraphe
+    Necessité : néant
+    Sortie : néant
+    Entraine : l'objet en paramètre est recopié et initialisé dans un nouvel objet
+    *****************************/
+    explicit CGrapheOperation(CGrapheOperation & GRAgrapheOperation);
+ 
+    /*****************************
+    Constructeur de confort
+    ******************************
+    Entrée : unsigned int uiNbSommets, unsigned int uiNbArcs
+    Necessité : néant
+    Sortie : néant
+    Entraine : l'objet en cours est initialisé
+    *****************************/
+    explicit CGrapheOperation(unsigned int uiNbSommets, unsigned int uiNbArcs);
+ 
+    /*****************************
+    Surcharge de l'operateur =
+    ******************************
+    Entrée : CGrapheOperation & GRAGrapheOperation
+    Necessité : néant
+    Sortie : CGrapheOperation &
+    Entraine : modification de l'objet a gauche du signe
+    *****************************/
+    CGrapheOperation & CGrapheOperation::operator=(CGrapheOperation & GRAGrapheOperation);
+ 
+    /*****************************
+    Destructeur par défaut
+    ******************************
+    Entrée : néant
+    Necessité : néant
+    Sortie : néant
+    Entraine : l'objet est détruit
+    *****************************/
+    ~CGrapheOperation();
+ 
+    // Methodes
+ 
+    /*****************************
+    Methode : Inverser un graphe
+    ******************************
+    Entrée : neant
+    Necessité : neant
+    Sortie : CGrapheOperation *
+    Entraine : le graphe de sortie est inverse
+    *****************************/
+    CGrapheOperation * GRAInverserGraphe();
 };
 #endif
